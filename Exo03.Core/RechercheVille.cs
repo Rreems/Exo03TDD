e8676df2114438bd8cfb571af792cb5e83e21cb3
Walen <walen212@gmail.com>
2025-02-07 12:35:23 +0100

    Exo03 VS Setup

---
+++ Exo03.Core/RechercheVille.cs
@@ -0,0 +1,11 @@
+﻿namespace Exo03.Core;
+
+public class RechercheVille
+{
+    private List<String> _villes;
+
+    public List<String> Rechercher(String mot)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Exo03.Tests/RechercheVilleTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Exo03.Tests
+{
+    
+    public sealed class RechercheVilleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
